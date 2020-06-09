4e1df9370989a0df32c1bc43b23ad6d1300a9715
UMESHRAUT <rautumesh34@gmail.com>
2020-06-09 16:03:27 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import KeyPad from "./components/KeyPad";
+import OutPut from "./components/output";
 
-function App() {
+
+
+class App extends Component {
+  state={
+    result:""
+  };
+
+  buttonPressed= buttonName =>{
+    if(buttonName==="="){
+      this.calculate();
+    }else
+    if(buttonName==="C"){
+      this.setState({
+        result:""
+      })
+    }
+    else if(buttonName==="CE"){
+      this.setState({
+        result:this.state.result.slice(0,-1)
+      })
+    }else if(this.state.result==="Math error")
+    this.setState({
+      result:""+buttonName
+    });
+    else
+    this.setState({
+      result:this.state.result+buttonName
+    });
+  }
+
+
+  calculate=()=>{
+    try{
+      this.setState({
+      result:eval(this.state.result)
+    })
+  }catch(e){
+    this.setState({
+      result:"Math error"
+    })
+  }
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App calc-body">
+
+      <OutPut result={this.state.result}/>
+      <KeyPad buttonPressed={this.buttonPressed}/>
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/output.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Output=(props)=>(
+    <div className="result">
+        <p>
+            {props.result}
+        </p>
+    </div>
+)
+
+export default Output;
